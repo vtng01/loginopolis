ebb772418f0302280817eac66d6c390a2a858246
Van Nguyen <vanthoai@fb.com>
2023-04-11 17:08:00 +0100

    complete base exercise

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { User } = require('./db');
+const { User } = require("./db");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res, next) => {
+app.get("/", async (req, res, next) => {
   try {
-    res.send('<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>');
+    res.send(
+      "<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>"
+    );
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
-
+app.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (username && password) {
+      const hash = await bcrypt.hash(password, 5);
+      await User.create({ username: username, password: hash });
+      res.send(`successfully created user ${username}`);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return "Failed";
+    }
+
+    const [foundUser] = await User.findAll({ where: { username } });
+
+    if (!foundUser) {
+      return "Failed";
+    }
+
+    const isMatch = await bcrypt.compare(password, foundUser.password);
+
+    if (isMatch) {
+      res.send(`successfully logged in user ${username}`);
+    } else {
+      res.status(401).send("incorrect username or password");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
--- index.test.js
+++ index.test.js
@@ -1,68 +1,81 @@
-const app = require('./index');
-const { sequelize, User } = require('./db');
-const request = require('supertest');
-const seed = require('./db/seedFn');
-const seedData = require('./db/seedData');
+const app = require("./index");
+const { sequelize, User } = require("./db");
+const request = require("supertest");
+const seed = require("./db/seedFn");
+const seedData = require("./db/seedData");
 
+describe("Endpoints", () => {
+  const testUserData = { username: "bobbysmiles", password: "youllneverguess" };
+  let registerResponse;
+  let loginResponse;
 
-describe('Endpoints', () => {
-    const testUserData = { username: 'bobbysmiles', password: 'youllneverguess' };
-    let registerResponse;
-    let loginResponse;
-    
-    beforeAll(async () => {
-        await sequelize.sync({ force: true }); // recreate db
-        await seed();
-        registerResponse = await request(app)
-            .post('/register')
-            .send(testUserData)
-            .catch(err => console.error(err));
-        loginResponse = await request(app)
-            .post('/login')
-            .send(testUserData)
-            .catch(err => console.error(err));
+  beforeAll(async () => {
+    await sequelize.sync({ force: true }); // recreate db
+    await seed();
+    registerResponse = await request(app)
+      .post("/register")
+      .send(testUserData)
+      .catch((err) => console.error(err));
+    loginResponse = await request(app)
+      .post("/login")
+      .send(testUserData)
+      .catch((err) => console.error(err));
+  });
+
+  describe("GET /", () => {
+    it("should return html", async () => {
+      const registerResponse = await request(app).get("/");
+      expect(registerResponse.status).toBe(200);
+      expect(registerResponse.text).toBe(
+        "<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>"
+      );
     });
+  });
 
-    describe('GET /', () => {
-        it('should return html', async () => {
-            const registerResponse = await request(app).get('/');
-            expect(registerResponse.status).toBe(200);
-            expect(registerResponse.text).toBe('<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>');
-        });
+  describe("POST /register", () => {
+    it("should send back success with username", async () => {
+      expect(registerResponse.status).toBe(200);
+      expect(registerResponse.text).toBe(
+        "successfully created user bobbysmiles"
+      );
     });
+    it("should create user with username", async () => {
+      const foundUser = await User.findOne({
+        where: { username: "bobbysmiles" },
+      });
+      expect(foundUser).toBeTruthy();
+      expect(foundUser.username).toBe("bobbysmiles");
+    });
+    it("should hash password", async () => {
+      const foundUser = await User.findOne({
+        where: { username: "bobbysmiles" },
+      });
+      expect(foundUser).toBeTruthy();
+      expect(foundUser.password).not.toBe(testUserData.password);
+      expect(foundUser.password).toEqual(
+        expect.stringMatching(/^\$2[ayb]\$.{56}$/)
+      );
+    });
+  });
 
-    describe('POST /register', () => {
-        it('should send back success with username', async () => {
-            expect(registerResponse.status).toBe(200);
-            expect(registerResponse.text).toBe('successfully created user bobbysmiles');
-        });
-        it('should create user with username', async () => {
-            const foundUser = await User.findOne({ where: { username: 'bobbysmiles' } });
-            expect(foundUser).toBeTruthy();
-            expect(foundUser.username).toBe('bobbysmiles');
-        });
-        it('should hash password', async () => {
-            const foundUser = await User.findOne({ where: { username: 'bobbysmiles' } });
-            expect(foundUser).toBeTruthy();
-            expect(foundUser.password).not.toBe(testUserData.password);
-            expect(foundUser.password).toEqual(expect.stringMatching(/^\$2[ayb]\$.{56}$/));
-        });
+  describe("POST /login", () => {
+    it("should send back success with username", async () => {
+      expect(loginResponse.status).toBe(200);
+      expect(loginResponse.text).toBe(
+        "successfully logged in user bobbysmiles"
+      );
     });
-
-    describe('POST /login', () => {
-        it('should send back success with username', async () => {
-            expect(loginResponse.status).toBe(200);
-            expect(loginResponse.text).toBe('successfully logged in user bobbysmiles');
-        });
-        it('if password incorrect, should send back 401 unauthorized, with message', async () => {
-            const incorrectLoginResponse = await request(app)
-                .post('/login')
-                .send({
-                    username: 'bobbysmiles',
-                    password: 'notright'
-                })
-                .catch(err => console.error(err));
-            expect(incorrectLoginResponse.text).toBe('incorrect username or password');
-        });
+    it("if password incorrect, should send back 401 unauthorized, with message", async () => {
+      const incorrectLoginResponse = await request(app)
+        .post("/login")
+        .send({
+          username: "bobbysmiles",
+          password: "notright",
+        })
+        .catch((err) => console.error(err));
+      expect(incorrectLoginResponse.text).toBe(
+        "incorrect username or password"
+      );
     });
+  });
 });
