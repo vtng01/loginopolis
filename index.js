7d28a5a0f5756d1629fe49cbfbba88be8f59d6c3
Van Nguyen <vanthoai@fb.com>
2023-04-12 10:00:40 +0100

    update bcrypt to bcryptjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { User } = require("./db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
