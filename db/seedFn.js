b18cbb729eb6f77a40c04beea5f00e61268fc67b
Van Nguyen <vanthoai@fb.com>
2023-04-12 10:58:46 +0100

    hashed seed data

--- db/seedFn.js
+++ db/seedFn.js
@@ -1,10 +1,18 @@
-const {sequelize} = require('./db');
-const {User} = require('./');
-const users = require('./seedData');
+const { sequelize } = require("./db");
+const { User } = require("./");
+const users = require("./seedData");
+const bcrypt = require("bcryptjs");
 
 const seed = async () => {
   await sequelize.sync({ force: true }); // recreate db
-  await User.bulkCreate(users);
+
+  const hashedUsers = await Promise.all(
+    users.map(async (user) => {
+      const hash = await bcrypt.hash(user.password, 10);
+      return { username: user.username, password: hash };
+    })
+  );
+  await User.bulkCreate(hashedUsers);
 };
 
 module.exports = seed;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const app = require('./index');
-const { sequelize } = require('./db');
+const app = require("./index");
+const { sequelize } = require("./db");
+const seed = require("./db/seedFn");
 
 const { PORT = 4000 } = process.env;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   sequelize.sync({ force: false });
+  await seed();
   console.log(`Users are ready at http://localhost:${PORT}`);
 });
